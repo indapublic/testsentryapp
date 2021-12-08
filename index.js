58b5c0a53a752fadd3cf7d1e0b837727bf0f28bb
Vasilii <vasilii.zolotukhin@gmail.com>
2021-12-08 15:48:14 +1000

    Fixed launch. Bumped version

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
  import * as Sentry from "@sentry/react-native";
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+import {App} from './App';
 import {name as appName} from './app.json';
 
 Sentry.init({
